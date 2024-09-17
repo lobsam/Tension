8dac9edc74b33b529b8eba44c33b9b278aac72bf
lobsam <sanduplobzang@gmail.com>
2024-09-17 23:32:29 +0530

    expose host

--- vite.config.js
+++ vite.config.js
@@ -6,13 +6,15 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueJsx(),
-  ],
+  plugins: [vue(), vueJsx()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
+  },
+  server: {
+    port: 3000, // Specify a port (or use any preferred port)
+    host: true, // Expose the server to the network
+    strictPort: true // Prevent fallback to other ports if this one is taken
   }
 })
